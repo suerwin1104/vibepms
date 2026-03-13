93952cac6feffee9758b2bc50ebe9307c28d03ab
suerwin1104 <suyuhsien@gmail.com>
2026-03-13 19:17:14 +0800

    Delete check_hotels.ts

--- check_hotels.ts
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-const supabase = createClient('https://uybkdnhaqnjrpwyqrocp.supabase.co', 'sb_publishable_h5Q1q1X5ISgW7K73q09dEg_npZq69Lb');
-async function check() {
-    const { data } = await supabase.from('hotels').select('*');
-    console.log(JSON.stringify(data, null, 2));
-}
-check();
